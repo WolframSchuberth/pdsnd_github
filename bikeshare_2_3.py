3b17297d14e691db559ab738eb0540839a87747e
Wolfram Schuberth <wolfram.schuberth@gmail.com>
2020-02-10 15:45:15 +0100

    Added Version description

--- bikeshare_2_3.py
+++ bikeshare_2_3.py
@@ -1,7 +1,10 @@
 import time
 import pandas as pd
 import numpy as np
+"""
+This is version 2.3 of BDAT (bikeshare analyses tool)
 
+"""
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
